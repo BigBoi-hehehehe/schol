a250163430fd6970bd78a8806bc94e700adc479c
BigBoi-hehehehe <1983@khaitanpublicschool.com>
2026-04-28 02:50:45 +0000

    g

---
+++ main.py
@@ -0,0 +1,10 @@
+import steamlit as st
+
+st.title('Mooo')
+st.write('Cow')
+
+name = st.text_input('Name do: ')
+age = st.number_input('Age do:')
+
+if st.button('Submit'):
+    st.write('Hello, {name}. Just leave :(')
